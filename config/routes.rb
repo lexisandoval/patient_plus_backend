ff63b8135c61332d13b62bf2eb7df9da6c348867
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-03 12:51:02 -0500

    Updated routes to follow namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :medications
+      resources :emergencycontacts
+      resources :conditions
+      resources :doctors
     end
   end
 
