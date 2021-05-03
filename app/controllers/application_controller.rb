e0603f934ecb5b5019f445aed7cfa1e3ab4e12ac
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-03 14:26:15 -0500

    Added current_user and logged_in? helpers to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+  helpers do
+    
+    def current_user
+      # change this
+      User.first
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+  end
+
 end
