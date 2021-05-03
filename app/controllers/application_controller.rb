05ce1c80b59214e29a3782e7c957f821f5133079
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-03 14:29:04 -0500

    Removed syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,12 @@
 class ApplicationController < ActionController::API
+  
+  def current_user
+    # change this
+    User.first
+  end
 
-  helpers do
-    
-    def current_user
-      # change this
-      User.first
-    end
-
-    def logged_in?
-      !!current_user
-    end
-
+  def logged_in?
+    !!current_user
   end
 
 end
