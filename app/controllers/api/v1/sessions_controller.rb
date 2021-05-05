e62e294a8de92c0f31e4927e526c79fbff1a21da
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-05 14:48:51 -0500

    Updated create action for session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: {
+        error: "Invalid"
+      }
+    end
+  end
+
 end
