5cef869edf5d2782e4d566e3d507afb53967b27e
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-11 11:19:27 -0500

    Added index action for doctors controller

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::DoctorsController < ApplicationController
+  before_action :set_doctor, only: [:show, :update, :destroy]
+
+  # GET /users
+  def index
+    # @doctors = Doctor.all
+    if logged_in?
+      @doctors = current_user.doctors
+
+      render json: @doctors
+    else
+      render json: {
+        error: "You must be logged in to view doctors."
+      }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def doctor_params
+    params.require(:doctor).permit(:name, :phone_number, :specialty, :location)
+  end
 end
