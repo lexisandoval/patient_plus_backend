9582d09dc68f7d395d3c2467de81b584ec3544c5
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-17 11:08:02 -0500

    Added index action and helpers to medications ccontroller

--- app/controllers/api/v1/medications_controller.rb
+++ app/controllers/api/v1/medications_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::MedicationsController < ApplicationController
+  before_action :set_doctor, only: [:show, :update, :destroy]
+
+  def index
+    # @doctors = Doctor.all
+    if logged_in?
+      @medications = current_user.medications
+
+      render json: MedicationSerializer.new(@medications)
+    else
+      render json: {
+        error: "You must be logged in to view medications."
+      }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_medications
+    @medication = Medication.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def medication_params
+    params.require(:medication).permit(:name, :prescription, :user_id, :condition_id, :doctor_id)
+  end
 end
