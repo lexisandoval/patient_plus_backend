19f59ccc09031ae7f569af061f2d5b109a966d7e
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-19 14:31:49 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
 
   has_secure_password
-
   has_many :emergencycontacts
   has_many :conditions
   has_many :medications
   has_many :doctors, through: :medications
+
+  validates :name, :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 
 end
