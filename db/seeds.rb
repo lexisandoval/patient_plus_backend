1530fe7834e4e8efe7ca2127e45f9a33def11639
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-05-21 17:31:24 -0500

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,14 @@ laura = User.create(name: "Laura Copeland", username: "lauracopeland", email: "l
 
 gregory = Doctor.create(name: "Gregory Zimmerman", phone_number: "214-749-8244", specialty: "Lipidologist", location: "Lakeland General Hospital")
 
+michael = Doctor.create(name: "Michael Miller", phone_number: "760-696-4879", specialty: "Cardiologist", location: "Fairbanks Medical Center")
+
 cholesterol = Condition.create(name: "High Cholesterol", user_id: 1)
+
+ra = Condition.create(name: "Rheumatoid Arthritis", user_id:1)
 
 elizabeth = Emergencycontact.create(name: "Elizabeth Copeland", phone_number: "678-999-8212", relationship: "mother", user_id: 1)
 
 simvastatin = Medication.create(name: "Simvastatin", prescription: "20 mg", user_id: 1, doctor_id: 1, condition_id: 1)
+
+humira = Medication.create(name: "Humira", prescription: "40 mg", user_id: 1, doctor_id: 2, condition_id: 2)
